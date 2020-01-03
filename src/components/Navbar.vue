98ed749a5572578155cb7ca3da2d83f46f5621b3
Riley Seaburg <footbllman62@systemadmin@veteranhosting.us>
2020-01-03 02:10:32 +0000

    changed nav color

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex items-center justify-between flex-wrap bg-blue-800g p-6">
+  <nav class="flex items-center justify-between flex-wrap bg-blue-800 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <g-link to="/">
         <span class="font-semibold text-xl tracking-tight">Riley For Congress</span>
